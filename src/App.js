e7be1dc3a0912d5618e547d210cf7fc93962d709
zarrinebrahimi22 <zarrinebrahimi22@gmail.com>
2024-08-29 23:18:03 -0700

    build all products card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/products/Products';
 
 function App() {
   return (
-  <div>
-    <h1 className="text-3xl font-bold underline  text-pink-500">
-      Hello world!
-    </h1>
+  <div  className='flex items-center justify-center bg-zinc-200 h-screen gap-x-6'>
+    <Products  title="China" price={12000}  image="./imges/bg-move-4.png"></Products>
+    <Products  title="Iran"  price={22000}  image="./imges/bg-move-6.png"></Products>
+    <Products  title="France" price={3000}  image="./imges/bg-move-9.png"></Products>
+    <Products  title="America"  price={89000} image="./imges/bg-move-10.png"></Products>
   </div>
    
   );
